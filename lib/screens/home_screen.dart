521e1dd76ea34c82eeac85963cfb1d3e6cd389e9
fardeenhimself <fardeenviper77@gmail.com>
2025-07-27 00:54:17 +0600

    List of todos shown

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_todo/providers/todo_provider.dart';
+import 'package:flutter_todo/utils/add_todo_box.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todos = ref.watch(todoListProvider);
     return Scaffold(
       appBar: AppBar(title: Text('ToDo\'s')),
-      body: Column(children: [Card(child: Text('Hello World'))]),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            final todo = todos[index];
+            return ListTile(
+              title: Text(todo.name),
+              subtitle: Text((todo.addedAt).toString()),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+            );
+          },
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showDialog(context: context, builder: (context) => AddTodoBox());
+        },
         child: Icon(Icons.add),
       ),
     );
---
+++ lib/utils/add_todo_box.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_todo/providers/todo_provider.dart';
+
+class AddTodoBox extends ConsumerWidget {
+  const AddTodoBox({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller = TextEditingController();
+    return AlertDialog(
+      title: const Text('Add New Todo'),
+      content: TextField(
+        controller: controller,
+        decoration: InputDecoration(hintText: 'Enter task name'),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () {
+            if (controller.text.trim().isNotEmpty) {
+              ref
+                  .read(todoListProvider.notifier)
+                  .addTodo(controller.text.trim());
+              Navigator.of(context).pop();
+            } else {
+              Text('Fild cannot be empty');
+            }
+          },
+          child: Text('Create'),
+        ),
+      ],
+    );
+  }
+}
